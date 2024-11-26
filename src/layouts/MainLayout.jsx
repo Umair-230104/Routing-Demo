6524ed6e455b6893172fc62ebca83b8e8ce6e001
Umair-230104 <cph-ut38@cphbusiness.dk>
2024-11-26 15:59:55 +0100

    3 commit

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,38 +1,40 @@
-import { NavLink, Outlet } from 'react-router'
+import { NavLink, Outlet, Link } from "react-router";
 
 function MainLayout() {
   return (
-    <div className='container'>
-      <header className='header'>
-        <img src='teamBlogo.png' alt='Logo' className='logo' />
+    <div className="container">
+      <header className="header">
+      <Link to="/">
+          <img src="teamBlogo.png" alt="Logo" className="logo" />
+        </Link>
       </header>
-      <div className='content'>
-        <nav className='sidebar'>
+      <div className="content">
+        <nav className="sidebar">
           <ul>
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <NavLink to='/products'>Products</NavLink>
+              <NavLink to="/products">Products</NavLink>
             </li>
             <li>
-              <NavLink to='/about'>About</NavLink>
+              <NavLink to="/about">About</NavLink>
             </li>
             <li>
-              <NavLink to='/contact'>Contact</NavLink>
+              <NavLink to="/contact">Contact</NavLink>
             </li>
           </ul>
         </nav>
-        <main className='main-content'>
+        <main className="main-content">
           <Outlet />
         </main>
       </div>
-      <footer className='footer'>
-        {' '}
+      <footer className="footer">
+        {" "}
         <p>&copy; 2024 Your Company Name. All rights reserved.</p>
       </footer>
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
